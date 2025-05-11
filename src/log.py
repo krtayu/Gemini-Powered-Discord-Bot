1e49542453d89d2b07b640c82dda8b135f057064
Krtayu <lovelylayla206+krtnu@gmail.com>
2025-05-12 00:48:36 +0530

    Update log.py

--- src/log.py
+++ src/log.py
@@ -25,34 +25,29 @@ class CustomFormatter(logging.Formatter):
         if formatter is None:
             formatter = self.FORMATS[logging.DEBUG]
 
-        # Override the traceback to always print in red
+        
         if record.exc_info:
             text = formatter.formatException(record.exc_info)
             record.exc_text = f'\x1b[31m{text}\x1b[0m'
 
         output = formatter.format(record)
 
-        # Remove the cache layer
         record.exc_text = None
         return output
 
 
 def setup_logger(module_name:str) -> logging.Logger:
-    # create logger
     library, _, _ = module_name.partition('.py')
     logger = logging.getLogger(library)
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
-        # create console handler
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(CustomFormatter())
-        # specify that the log file path is the same as `main.py` file path
         grandparent_dir = os.path.abspath(__file__ + "/../../")
         log_name='discord_bot.log'
         log_path = os.path.join(grandparent_dir, log_name)
-        # create local log handler
         log_handler = logging.handlers.RotatingFileHandler(
             filename=log_path,
             encoding='utf-8',
@@ -60,7 +55,6 @@ def setup_logger(module_name:str) -> logging.Logger:
             backupCount=2,  # Rotate through 5 files
         )
         log_handler.setFormatter(CustomFormatter())
-        # Add handlers to logger
         logger.addHandler(log_handler)
         logger.addHandler(console_handler)
 
